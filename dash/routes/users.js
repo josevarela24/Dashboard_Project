f51173c647ffaeb3e6adafda1aaebe24c1cdce49
Jose Varela <jose.varela24@yahoo.com>
2018-02-02 14:08:44 -0600

    modified users.js route, created trial chart

--- dash/routes/users.js
+++ dash/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var vdd = require('../vd.json')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //console.log(vdd);
+  res.render('random', { title: 'Random', vdd: vdd });
 });
 
 module.exports = router;
