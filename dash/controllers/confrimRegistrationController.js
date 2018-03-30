95acba997bbe54f88a02c6c4e83f7ef08989638b
unknown <naomihbeltrand@gmail.com>
2018-03-30 13:34:28 -0500

    Jose is Wrong

---
+++ dash/controllers/confrimRegistrationController.js
@@ -0,0 +1,89 @@
+var csv = require('fast-csv');
+var mongoose = require('mongoose');
+var userLogin = require('../models/userLogin');
+//*fixed..this upload does not delete what is already inside db, need to implement drop feature
+var async = require('async');
+
+exports.get_detail = function(req, res, next) {
+	
+	async.parallel({
+		one: function(callback){
+			Country.aggregate([{$sort: { email: 1 }},{
+					$group: {
+						_id: '$name',
+						nominalGDP: {$push: "$nominalGDP"}
+					}
+				}, 
+				{
+					$project: {
+					_id: 0,
+					name: "$_id",
+					'data' : '$nominalGDP'
+					}
+				}
+				], callback);
+		},
+
+		two: function(callback){
+			Country.aggregate([
+				{			
+					$match: {
+						// $and: [
+						// 	{name: { $in: ["USA", "China", "India", "France", "Japan"]}},
+						// 	{year: 2017}
+						// ]
+						year: 2014
+					}
+				},
+				{
+					$sort: {
+						"nominalGDP": -1
+					}
+				},
+				{
+					$project: {
+					_id: 0,
+					name: 1,
+					'y' : '$nominalGDP'
+					}
+				}		
+				], callback);
+		}
+	}, function(err, results){
+		console.log(results.one);
+		console.log(results.two);
+		res.render('hello', { title: 'Test', data: results.one, pie: results.two });
+	});
+}
+
+exports.post_detail = function (req, res, next) {
+	if (!req.files){
+		return res.status(400).send('No files were uploaded.');
+    }
+	var countryFile = req.files.file;
+	//console.log(countryFile);
+	var countries = [];
+		
+	csv
+	 .fromString(countryFile.data.toString(), {
+		 headers: true,
+		 ignoreEmpty: true
+	 })
+	 .on("data", function(data){
+		 data['_id'] = new mongoose.Types.ObjectId();
+		 
+		 countries.push(data);
+	 })
+	 .on("end", function(){
+		//this removes documents only on first upload***
+		//fixed! now removes documents on each
+		Country.remove({}, function(err,removed) {});
+
+		 Country.create(countries, function(err, documents) {
+			if (err) throw err;
+			//console.log(countries);
+			//res.send(countries.length + ' countries have been successfully uploaded.');
+			res.redirect('/users');
+		});
+	 });
+};
