0188ff327db1d4f673e0ba2f423228734872adda
Katie Schliesing <katieschliesing@cometnet-10-21-50-23.utdallas.edu>
2018-03-30 13:42:52 -0500

    test

--- dash/controllers/uploadController.js
+++ dash/controllers/uploadController.js
@@ -8,13 +8,14 @@ var async = require('async');
 exports.get_detail = function(req, res, next) {
 	
 	async.parallel({
+		/*
 		one: function(callback){
 			Country.aggregate([
 				/* {
 					$match: {
 					name: { $in: ["USA", "China", "India", "France", "Japan"]},
 					}
-				}, */
+				}, 
 				{
 					$sort: {
 						year: 1
@@ -35,8 +36,8 @@ exports.get_detail = function(req, res, next) {
 				}
 				], callback);
 		},
-
-		two: function(callback){
+		*/
+		one: function(callback){
 			Country.aggregate([
 				{			
 					$match: {
@@ -60,14 +61,232 @@ exports.get_detail = function(req, res, next) {
 					}
 				}		
 				], callback);
+		},
+
+		two: function(callback){
+			Country.aggregate([
+				{			
+					$match: {
+						// $and: [
+						// 	{name: { $in: ["USA", "China", "India", "France", "Japan"]}},
+						// 	{year: 2017}
+						// ]
+						year: 2014
+					}
+				},
+				{
+					$sort: {
+						"gdpPpp": -1
+					}
+				},
+				{
+					$project: {
+					_id: 0,
+					name: 1,
+					'y' : '$gdpPpp'
+					}
+				}		
+				], callback);
+		},
+
+		three: function(callback){
+			Country.aggregate([
+				{			
+					$match: {
+						// $and: [
+						// 	{name: { $in: ["USA", "China", "India", "France", "Japan"]}},
+						// 	{year: 2017}
+						// ]
+						year: 2014
+					}
+				},
+				{
+					$sort: {
+						"population": -1
+					}
+				},
+				{
+					$project: {
+					_id: 0,
+					name: 1,
+					'y' : '$population'
+					}
+				}		
+				], callback);
+		},
+
+		four: function(callback){
+			Country.aggregate([
+				{			
+					$match: {
+						// $and: [
+						// 	{name: { $in: ["USA", "China", "India", "France", "Japan"]}},
+						// 	{year: 2017}
+						// ]
+						year: 2014
+					}
+				},
+				{
+					$sort: {
+						"nominalGDP": -1
+					}
+				},
+				{
+					$project: {
+					_id: 0,
+					name: 1,
+					'y' : '$nominalGDP'
+					}
+				}		
+				], callback);
+		},
+
+		
+		five: function(callback){
+			Country.aggregate([
+				{			
+					$match: {
+						// $and: [
+						// 	{name: { $in: ["USA", "China", "India", "France", "Japan"]}},
+						// 	{year: 2017}
+						// ]
+						year: 2014
+					}
+				},
+				{
+					$sort: {
+						"nominalGDP": -1
+					}
+				},
+				{
+					$project: {
+					_id: 0,
+					name: 1,
+					'y' : '$nominalGDP'
+					}
+				}		
+				], callback);
+		},
+
+		ten: function(callback){
+			Country.aggregate([
+				{
+					$match: {
+					name: { $in: ["USA", "Japan", "Canada"]},
+					}
+				}, 
+				{
+					$sort: {
+						year: 1
+					}
+				},
+				{
+					$group: {
+						_id: '$name',
+						realGDPGrowth: {$push: "$realGDPGrowth"}
+					}
+				}, 
+				{
+					$project: {
+					_id: 0,
+					name: "$_id",
+					'data' : '$realGDPGrowth'
+					}
+				}
+				], callback);
+		},
+
+		eleven: function(callback){
+			Country.aggregate([
+				{
+					$match: {
+					name: { $in: ["France", "Germany", "Italy", "UK"]},
+					}
+				}, 
+				{
+					$sort: {
+						year: 1
+					}
+				},
+				{
+					$group: {
+						_id: '$name',
+						realGDPGrowth: {$push: "$realGDPGrowth"}
+					}
+				}, 
+				{
+					$project: {
+					_id: 0,
+					name: "$_id",
+					'data' : '$realGDPGrowth'
+					}
+				}
+				], callback);
+		},
+
+		twelve: function(callback){
+			Country.aggregate([
+				{
+					$match: {
+					name: { $in: ["USA", "Japan", "Canada"]},
+					}
+				}, 
+				{
+					$sort: {
+						year: 1
+					}
+				},
+				{
+					$group: {
+						_id: '$name',
+						realGDPGrowth: {$push: "$realGDPGrowth"}
+					}
+				}, 
+				{
+					$project: {
+					_id: 0,
+					name: "$_id",
+					'data' : '$realGDPGrowth'
+					}
+				}
+				], callback);
+		},
+
+		thirteen: function(callback){
+			Country.aggregate([
+				{
+					$match: {
+					name: { $in: ["France", "Germany", "Italy", "UK"]},
+					}
+				}, 
+				{
+					$sort: {
+						year: 1
+					}
+				},
+				{
+					$group: {
+						_id: '$name',
+						realGDPGrowth: {$push: "$realGDPGrowth"}
+					}
+				}, 
+				{
+					$project: {
+					_id: 0,
+					name: "$_id",
+					'data' : '$realGDPGrowth'
+					}
+				}
+				], callback);
 		}
+
 	}, function(err, results){
 		if(res.locals.admin){
 			console.log("I am admin")
 			res.render('admin', { title: 'Test', data: results.one, pie: results.two });
 		} else {
 			console.log("I am NOT admin")
-			res.render('hello', { title: 'Test', data: results.one, pie: results.two });
+			res.render('hello', { title: 'Test', p1: results.one, p2: results.two, p3: results.three, graph:results.four, spend:results.five, l2: results.ten, l3: results.eleven});
 		}
 		// console.log(results.one);
 		// console.log(results.two);
