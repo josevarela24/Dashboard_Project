79a1c67fbbc8f4456b6624a483a8f63452d0f834
Jose Varela <jose.varela24@yahoo.com>
2018-03-31 13:06:34 -0500

    changed to highmaps

--- dash/public/javascripts/scripts.js
+++ dash/public/javascripts/scripts.js
@@ -376,7 +376,7 @@ function cSpend(spend){
     });
 }
 
-function getMap(){
+/* function getMap(){
     console.log("Here at map");
     //basic map config with custom fills, mercator projection
     var map = new Datamap({
@@ -414,6 +414,77 @@ function getMap(){
         });
     }
     })
+} */
+
+function getMap(){
+    console.log("Here at map");
+    $.getJSON('https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/world-population-density.json', function (data) {
+
+        // Prevent logarithmic errors in color calulcation
+        $.each(data, function () {
+            this.value = (this.value < 1 ? 1 : this.value);
+        });
+
+        // Initiate the chart
+        Highcharts.mapChart('random', {
+
+            chart: {
+                map: 'custom/world'
+            },
+
+            title: {
+                text: 'Population Density'
+            },
+
+            legend: {
+                title: {
+                    text: 'Population density per km²',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                    }
+                }
+            },
+
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+
+            tooltip: {
+                backgroundColor: 'none',
+                borderWidth: 0,
+                shadow: false,
+                useHTML: true,
+                padding: 10,
+                pointFormat: '<span class="f32"><span class="flag {point.properties.hc-key}">' +
+                    '</span></span> {point.name}<br>' +
+                    '<span style="font-size:30px">{point.value}/km²</span>',
+                positioner: function () {
+                    return { x: 0, y: 250 };
+                }
+            },
+
+            colorAxis: {
+                min: 1,
+                max: 1000,
+                type: 'logarithmic'
+            },
+
+            series: [{
+                data: data,
+                joinBy: ['iso-a3', 'code3'],
+                name: 'Population density',
+                states: {
+                    hover: {
+                        color: '#a4edba'
+                    }
+                }
+            }]
+        })
+    });
+
 }
 
 /* function getWorld(){
